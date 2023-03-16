517b52f13e85f9169dbd48c3ccfd6e3c87b7dcf7
SamuelArico99 <samuel16699.sa@gmail.com>
2023-03-16 18:27:10 +0100

    Rappresentazione DD dei fumetti

--- database/seeders/ComicsTableSeeder.php
+++ database/seeders/ComicsTableSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Comic;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+
 
 class ComicsTableSeeder extends Seeder
 {
@@ -16,8 +18,16 @@ class ComicsTableSeeder extends Seeder
     {
         $comics = config('comics');
 
-        foreach ($comics as $index => $comic) {
-            dd($comic);
+        foreach ($comics as $comic) {
+            $newComic = new Comic;
+            $newComic->title = $comic['title'];
+            $newComic->description = $comic['description'];
+            $newComic->thumb = $comic['thumb'];
+            $newComic->price = $comic['price'];
+            $newComic->series = $comic['series'];
+            $newComic->sale_date = $comic['sale_date'];
+            $newComic->type = $comic['type'];
+            $newComic->save();
         }
     }
 }
