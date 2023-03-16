943d906d70ff068de9f9a5629473aa1802ae1d46
SamuelArico99 <samuel16699.sa@gmail.com>
2023-03-16 16:57:07 +0100

    Modifica Controller,Creazione Seeder, Creazione Database, Creazione Model

--- app/Http/Controllers/Admin/ComicController.php
+++ app/Http/Controllers/Admin/ComicController.php
@@ -1,10 +1,11 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Admin;
 
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class TestController extends Controller
+class ComicController extends Controller
 {
     /**
      * Display a listing of the resource.
